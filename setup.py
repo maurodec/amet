afbd66a4e0a82c4292309ef6d875af8653610308
Mauro de Carvalho <mauro.dec@gmail.com>
2017-04-20 18:36:52 -0300

    Bumped version to 0.3.

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup (
     name='amet',
     packages=['amet'],
-    version='0.2',
+    version='0.3',
     description='A library to read complex configuration from environment variables.',
     author='Mauro de Carvalho',
     author_email='mauro.dec@gmail.com',
     url='https://github.com/maurodec/amet',
-    download_url='https://github.com/maurodec/amet/archive/0.2.tar.gz',
+    download_url='https://github.com/maurodec/amet/archive/0.3.tar.gz',
     keywords=['configuration', 'environment'],
     classifiers=[],
 )
